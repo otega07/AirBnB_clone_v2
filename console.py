9f1de85575df32b6a9102d3dce9484b2496ed0c5
otega07 <okiriotega@gmail.com>
2024-05-07 14:44:20 +0100

    storages

--- console.py
+++ console.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" Console Module """
+""" console """
 
 import cmd
 from datetime import datetime
 import models
-from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.state import State
 from models.city import City
 from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
+from models.amenity import Amenity
+from models.review import Review
 import shlex  # for splitting the line along spaces except in double quotes
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"BaseModel": BaseModel, "State": State,
+           "City": City, "Place": Place, "User": User, "Amenity": Amenity, "Review": Review}
 
 
 class HBNBCommand(cmd.Cmd):
